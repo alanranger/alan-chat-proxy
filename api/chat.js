e20294c9d16132465e0828b9a6cfe40fee3adfb4
alanranger <info@alanranger.com>
2025-10-14 23:10:39 +0100

    FIX: Add missing type field to events responses

--- api/chat.js
+++ api/chat.js
@@ -3758,6 +3758,7 @@ export default async function handler(req, res) {
 
       res.status(200).json({
         ok: true,
+        type: "events",
         answer_markdown: answerMarkdown,
         citations,
         structured: {
