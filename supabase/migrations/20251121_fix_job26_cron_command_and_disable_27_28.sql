6d0a6cfe22e46385dcf5b8ed82371839452267e6
alanranger <info@alanranger.com>
2025-11-21 14:52:20 +0000

    Update migration to use cron.alter_job function
    - Changed from direct UPDATE statements to using cron.alter_job() function
    - This is the proper way to modify cron jobs in Supabase
    - Jobs 26, 27, and 28 have already been updated in the database

--- supabase/migrations/20251121_fix_job26_cron_command_and_disable_27_28.sql
+++ supabase/migrations/20251121_fix_job26_cron_command_and_disable_27_28.sql
@@ -3,16 +3,21 @@
 -- Jobs 27 and 28 should be disabled since they're chained internally by job 26
 
 -- Fix job 26 to use the correct function
-UPDATE cron.job
-SET command = 'SELECT trigger_refresh_master_job();'
-WHERE jobid = 26;
+SELECT cron.alter_job(
+  job_id := 26,
+  command := 'SELECT trigger_refresh_master_job();'
+);
 
 -- Disable jobs 27 and 28 since they're handled internally by job 26
 -- These jobs will still exist but won't run on schedule
 -- They can be manually triggered if needed, but normally job 26 handles everything
-UPDATE cron.job
-SET active = false
-WHERE jobid IN (27, 28);
+SELECT cron.alter_job(
+  job_id := 27,
+  active := false
+);
 
-COMMENT ON TABLE cron.job IS 'Job 26 runs trigger_refresh_master_job() which internally chains batches 0->1->2. Jobs 27 and 28 are disabled since they are handled internally.';
+SELECT cron.alter_job(
+  job_id := 28,
+  active := false
+);
 
